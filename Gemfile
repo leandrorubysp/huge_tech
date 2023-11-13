3d461c88137b67d690f30d1b12759aca2e7bbf74
Leandro <leandro.ruby@hotmail.com>
2023-11-13 13:28:05 -0300

    Installing Sidekiq gem

--- Gemfile
+++ Gemfile
@@ -35,6 +35,9 @@ gem "jbuilder"
 # Use Redis adapter to run Action Cable in production
 gem "redis", ">= 4.0.1"
 
+# Sidekiq
+gem 'sidekiq'
+
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,20 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_options = {from: 'huge.tech0@gmail.com'}
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'huge.tech0@gmail.com',
+    password:             'M3186870',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
